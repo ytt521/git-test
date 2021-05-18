ed5451446da483224fb4f5cc3dd3c2b4201936aa
ws <3047893225@qq.com>
2021-05-18 23:24:19 +0800

    first commit

---
+++ src/main/java/com/ws/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ws.git;
+
+/**
+ * @author 挚爱小v脸
+ * @create 2021-05-18 下午 23:20
+ * @since 1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
