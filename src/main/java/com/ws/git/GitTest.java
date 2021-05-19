8a6283e868893a1343d95cf5b43bf4fe02ec59a1
ws <3047893225@qq.com>
2021-05-19 09:24:14 +0800

    third commit

--- src/main/java/com/ws/git/GitTest.java
+++ src/main/java/com/ws/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
